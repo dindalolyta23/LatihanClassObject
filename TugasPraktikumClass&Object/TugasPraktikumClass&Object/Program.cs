9806c94aa9dce6466e6d4f48ec5e92b78eaa63c7
dindalolyta23 <dindalolyta@students.amikom.ac.id>
2023-04-05 16:47:32 +0700

    LatihanClassObject

---
+++ TugasPraktikumClass&Object/TugasPraktikumClass&Object/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasPraktikumClass_Object;
+
+namespace praktek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Objek Taxi
+            Taxi taxi = new Taxi();
+
+            // Pengesetan Nilai Properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // Pemanggilan Method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
